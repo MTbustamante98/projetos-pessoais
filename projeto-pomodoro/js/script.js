af7101354024592799a9198425dec53fef9ece96
Matheus Bustamante <139372893+MTbustamante98@users.noreply.github.com>
2025-07-07 00:12:25 -0300

    update

--- projeto-pomodoro/js/script.js
+++ projeto-pomodoro/js/script.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const modal = document.querySelector(".modal");
   const configs = document.querySelector(".configs");
   const menu = document.querySelector(".menu");
-  console.log(menu);
   const containerTasks = document.querySelector(".add-list-tasks");
   const divAddTasks = document.querySelector(".tasks");
 
@@ -29,7 +28,6 @@ document.addEventListener("DOMContentLoaded", () => {
       e.stopPropagation();
 
       const target = e.target;
-      console.log(target)
       if (target.hasAttribute("data-modal")) {
         modal.classList.add(active);
       }
@@ -43,7 +41,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
       if (target.closest("[data-menu]")) {
         menu.classList.toggle(active);
-        console.log(menu)
         modalLinks[2].classList.toggle("scale-effect");
       }
     }
@@ -85,7 +82,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.addEventListener("click", closeModal);
     modalLinks.forEach((el) => el.addEventListener("click", openModal));
-    imgClose.forEach((el) => el.addEventListener("click", imgCloseModalAndConfigs));
+    imgClose.forEach((el) =>
+      el.addEventListener("click", imgCloseModalAndConfigs)
+    );
   }
   initModals();
 
@@ -332,13 +331,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function initDropConfig() {
     const toggleDrop = document.querySelectorAll("[data-toggle-drop]");
+    const innerTextLiDrop = document.querySelectorAll(`[data-drop] li`);
+
     const callback = (e) => {
       const id = e.target.getAttribute("data-toggle-drop");
       const drop = document.querySelector(`[data-drop="${id}"]`);
-      if (drop) {
-        drop.classList.toggle(active);
-      }
+      if (drop) drop.classList.toggle(active);
     };
+
+    innerTextLiDrop.forEach((liText) => {
+      liText.addEventListener("click", (e) => {
+        const parentDrop = e.target.closest("[data-drop]");
+        if (!parentDrop) return;
+
+        const id = parentDrop.getAttribute("data-drop");
+        const toggleDrop = document.querySelector(`[data-toggle-drop="${id}"]`);
+        if (!toggleDrop) return;
+
+        const span = toggleDrop?.querySelector(".format-innerText");
+        if (span) span.innerText = e.target.innerText;
+
+        const drop = document.querySelectorAll("[data-drop]");
+        drop.forEach((dropDown) => {
+          if (dropDown.classList.contains(active)) dropDown.classList.remove(active);
+        })
+        
+      });
+    });
+
+    document.addEventListener("click", (e) => {
+      let clickInside = false;
+      document
+        .querySelectorAll("[data-toggle-drop], [data-drop]")
+        .forEach((el) => {
+          if (el.contains(e.target)) clickInside = true;
+        });
+
+      if (!clickInside) {
+        document.querySelectorAll("[data-drop]").forEach((drop) => {
+          drop.classList.remove(active);
+        });
+      }
+    });
 
     toggleDrop.forEach((div) => {
       div.addEventListener("click", callback);
